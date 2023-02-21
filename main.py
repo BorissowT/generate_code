e0304f346c17a0a5a2bcca67deead61db6e4666e
tb <tb@home-klick.de>
2023-02-21 18:17:06 +0100

    generate python scripts

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+from jinja2 import Environment, FileSystemLoader
+env = Environment(loader=FileSystemLoader('templates'))
+template = env.get_template('template_script.py')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def rewrite_to_camel_case(word):
+    words = word.split("_")
+    words = [x.capitalize() for x in words]
+    return "".join(words)
+
+
+def rewrite_to_lowercase_plural_docstrings(word):
+    words = word.split("_")
+    return " ".join(words)
+
+def rewrite_to_lowercase_singular_docstrings(word):
+    words = word.split("_")
+    words[-1] = words[-1][:-1]
+    return " ".join(words)
+
+
+def rewrite_to_docstrings_name_camelcase_plural(word):
+    words = word.split("_")
+    words = [x.capitalize() for x in words]
+    return " ".join(words)
+
+
+names = ["color_palettes",]
+
+for elem in names:
+    name = elem
+    camel_case_name = rewrite_to_camel_case(elem)
+    name_singular = elem[:-1]
+    docstrings_name_lower_case_plural =\
+        rewrite_to_lowercase_plural_docstrings(elem)
+    docstring_name_lower_case_singular =\
+        rewrite_to_lowercase_singular_docstrings(elem)
+    docstrings_name_camelcase_plural = \
+        rewrite_to_docstrings_name_camelcase_plural(elem)
+
+    output_from_parsed_template = template.render(name=name,
+                                                  camel_case_name=camel_case_name,
+                                                  name_singular=name_singular,
+                                                  docstrings_name_lower_case_plural=docstrings_name_lower_case_plural,
+                                                  docstring_name_lower_case_singular=docstring_name_lower_case_singular,
+                                                  docstrings_name_camelcase_plural=docstrings_name_camelcase_plural)
+
+    with open(f"templates/{name}.py", "w") as fh:
+        fh.write(output_from_parsed_template)
---
+++ templates/template_script.py
@@ -0,0 +1,136 @@
+"""{{ docstrings_name_camelcase_plural }} Controller.
+Controller class implements logic, collection of functions executed when api
+route is called.
+"""
+import log
+from base import shared
+from {{ name }}.{{ name }}_repository import {{ camel_case_name }}Repository
+
+
+{{ name }}_repository = {{ camel_case_name }}Repository()
+
+
+def load_{{ name }}():
+    """Load {{ docstring_name_lower_case_singular }} data."""
+    {{ name }}_repository.load_data_to_cache()
+
+
+load_{{ name }}()
+
+
+def save_{{ name }}():
+    """Docstring"""
+    {{ name }}_repository.save_elements()
+
+
+# =============================================================================
+
+
+def has_{{ name_singular }}({{ name_singular }}_id):
+    """Return True if {{ name_singular }} with id exists."""
+    return {{ name }}_repository.has_element({{ name_singular }}_id)
+
+
+def get_{{ name_singular }}({{ name_singular }}_id):
+    """Return {{ docstring_name_lower_case_singular }} specified by id."""
+    return {{ name }}_repository.get_element({{ name_singular }}_id)
+
+
+def get_{{ name }}():
+    """Return {{ docstrings_name_lower_case_plural }}."""
+    return {{ name }}_repository.get_data_from_cache()
+
+
+# =============================================================================
+
+
+def handle_get_{{ name }}(io_context, **kwargs):
+    """Return all {{ docstrings_name_lower_case_plural }}."""
+    return shared.json_get_handler(
+        {{ name }}_repository.get_data_from_cache(),
+        io_context
+    )
+
+
+def handle_get_{{ name_singular }}(io_context, matched_path, **kwargs):
+    """Return a specific {{ docstring_name_lower_case_singular }}."""
+    return shared.json_get_handler(
+        get_{{ name_singular }}(matched_path['item_id']),
+        io_context
+    )
+
+
+def handle_get_{{ name_singular }}_value(io_context, matched_path, **kwargs):
+    """Return value for key of a specific {{ docstring_name_lower_case_singular }}."""
+    return shared.json_get_handler(
+        getattr(
+            get_{{ name_singular }}(matched_path['item_id']),
+            matched_path['field_id']
+        ),
+        io_context
+    )
+
+
+shared.register_api_routes(
+    key='{{ name }}',
+    list_getter=handle_get_{{ name }},
+    item_getter=handle_get_{{ name_singular }},
+    value_getter=handle_get_{{ name_singular }}_value
+)
+
+
+# =============================================================================
+
+
+def maintain_module():
+    """Perform repeating maintenance tasks."""
+    log.info("saving {{ name }}...")
+    save_{{ name }}()
+
+
+# =============================================================================
+
+
+def test_functionality(result, out_progress, out_progress_end):
+    """Run tests for this module and return any message."""
+    count = 0
+    messages = []
+
+    if {{ name }}_repository.get_data_from_cache():
+        {{ name_singular }}_id = {{ name }}_repository.get_data_from_cache()[0].id
+
+        if not has_{{ name_singular }}({{ name_singular }}_id):
+            messages.append('has_{{ name_singular }} failed')
+        count = count + 1
+        out_progress(count)
+
+        if not get_{{ name_singular }}({{ name_singular }}_id) is {{ name }}_repository.get_data_from_cache()[0]:
+            messages.append('get_{{ name_singular }} failed')
+        count = count + 1
+        out_progress(count)
+
+        # TODO: test all functions
+
+    out_progress_end()
+
+    result['functionality_tests']['messages'] = messages
+    result['functionality_tests']['test_count'] = count
+    return result
+
+
+def test_validity(result, out_progress, out_progress_end):
+    """Run tests for this module and return any message."""
+    count = 0
+    messages = []
+
+    {{ name }}_len = len({{ name }}_repository.get_data_from_cache())
+    for {{ name_singular }} in {{ name }}_repository.get_data_from_cache():
+        test_count, test_messages = {{ name_singular }}.validate({{ name }}_repository.get_data_from_cache())
+        count = count + test_count
+        messages.extend(test_messages)
+        out_progress(count, {{ name }}_len)
+    out_progress_end()
+
+    result['validity_tests']['messages'] = messages
+    result['validity_tests']['test_count'] = count
+    return result
